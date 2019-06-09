e3af37558c1d4fc84b0bd887a541b36ceaf02108
ronkagan <ronkagan@users.noreply.github.com>
2019-06-09 16:31:32 -0400

    successfully tapped cask via brew to create an updated instance of chrome with which to run the scrape in the exercise

--- crawling_pages_with_selenium_1.py
+++ crawling_pages_with_selenium_1.py
@@ -1,8 +1,17 @@
 from selenium import webdriver
 
 # Open up a browser and head to a webpage
-driver = webdriver.Chromium()
+driver = webdriver.Chrome()
 driver.get("http://econpy.pythonanywhere.com/ex/001.html")
 
 # Extract lists of "buyers" and "prices" based on xpath
-buyers = driver.find_elements_by_xpath()
+buyers = driver.find_elements_by_xpath('//div[@title="buyer-name"]')
+prices = driver.find_elements_by_xpath('//span[@class="item-price"]')
+
+# Print out all of the buyers and prices on the current page
+num_page_items = len(buyers)
+for i in range(num_page_items):
+	print(buyers[i].text + " : " + prices[i].text)
+
+# Clean up (close browser once task is complete)
+driver.close()
