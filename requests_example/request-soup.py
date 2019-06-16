28fc13a55525021fafff96650bbd56e20ee02b7c
ronkagan <ronkagan@users.noreply.github.com>
2019-06-16 14:02:40 -0400

    finished 'BeautifulSoup and Requests in Python: Quickstart and Introduction'. Overview of attrs, sorting logic of pages.

--- requests_example/request-soup.py
+++ requests_example/request-soup.py
@@ -2,8 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 #https://newyork.craigslist.org/search/mnh/fbh?query=-uber+-pared+-doordash+-postmates&postedToday=1&bundleDuplicates=1
+#https://ny.eater.com/
 
-result = requests.get("https://ny.eater.com/")
+result = requests.get("https://www.whitehouse.gov/briefings-statements/")
 src = result.content
 soup = BeautifulSoup(src,'lxml')
 
